1828626e1fa0222c88e4b0c898218cf5b5b28c58
felpo <feliperangel897@gmail.com>
2024-01-15 19:30:22 -0300

    criado função criaQuadrado

--- script.js
+++ script.js
@@ -11,5 +11,17 @@ const jogador1 = criaJogador('Felipe Rangel', 'X')
 const jogador2 = criaJogador('Maria Eduarda', 'O')
 
 function selecionaQuadrado(id, jogador){
-    
+
 }
+
+function criaQuadrado() {
+    let container = document.querySelector('#container')
+    for (let i = 1; i < 10; i++) {
+        let quadrado = document.createElement('div')
+        quadrado.id = i
+        quadrado.addEventListener('click', () => selecionaQuadrado(this.id, jogadorDaVez))
+        container.appendChild(quadrado)
+    }
+}
+
+criaQuadrado()
