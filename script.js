096f9b9fc3f65ec2eee72dd3d11e4a38d9ef5848
felpo <feliperangel897@gmail.com>
2024-01-16 18:33:36 -0300

    refatorado o código, adicionado lógica pra função selecionaQuadrado e adicionado função trocaJogador

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-let jogadorDaVez // criar função pra selecionar qual o jogador da vez
-
-const jogo = {
-    tabuleiro: [[0, 0, 0], [0, 0, 0], [0, 0 ,0]]
-}
-
 function criaJogador(nome, marcador) {
     return {nome, marcador}
 }
@@ -12,8 +6,24 @@ function criaJogador(nome, marcador) {
 const jogador1 = criaJogador('Felipe Rangel', 'X')
 const jogador2 = criaJogador('Maria Eduarda', 'O')
 
-function selecionaQuadrado(id, jogador){
+let jogadorDaVez = jogador1// criar função pra selecionar qual o jogador da vez
 
+const jogo = {
+    tabuleiro: [[0, 0, 0], [0, 0, 0], [0, 0 ,0]]
+}
+
+function selecionaQuadrado(id, jogador){
+    let quadrado = document.querySelector(`#${id}`)
+    quadrado.innerHTML = jogador.marcador
+    trocaJogador(jogador)
+}
+
+function trocaJogador(jogador){
+    if (jogador === jogador1) {
+        jogadorDaVez = jogador2
+    } else {
+        jogadorDaVez = jogador1
+    }
 }
 
 // Função que cria os quadrados para marcar e adiciona a função que recebe o clique em cada um
@@ -21,7 +31,7 @@ function criaQuadrado() {
     let container = document.querySelector('#container')
     for (let i = 1; i < 10; i++) {
         let quadrado = document.createElement('div')
-        quadrado.id = i
+        quadrado.id = `num${i}`
         quadrado.addEventListener('click', () => selecionaQuadrado(quadrado.id, jogadorDaVez))
         container.appendChild(quadrado)
     }
