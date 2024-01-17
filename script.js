f2472d6fcca37a8eeebfa87f84f68ee0b38370d9
felpo <feliperangel897@gmail.com>
2024-01-17 20:45:59 -0300

    adicionado argumento a função checaVencedor para receber o objeto do jogador

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function selecionaQuadrado(id, jogador){
     let quadrado = document.querySelector(`#${id}`)
     quadrado.innerHTML = jogador.marcador
     jogo.tabuleiro[id[3]][id[4]] = jogador.num
-    checaVencedor(jogo.tabuleiro)
+    checaVencedor(jogo.tabuleiro, jogador)
     trocaJogador(jogador)
 }
 
@@ -28,23 +28,23 @@ function trocaJogador(jogador){
     }
 }
 
-function checaVencedor(tabuleiro){
+function checaVencedor(tabuleiro, jogador){
     if (tabuleiro[0][0] !== 0 && tabuleiro[0][0] === tabuleiro[0][1] && tabuleiro[0][0] === tabuleiro[0][2]){
-        console.log('ganhou')
+        console.log(`${jogador.nome} ganhou!`)
     } else if (tabuleiro[1][0] !== 0 && tabuleiro[1][0] === tabuleiro[1][1] && tabuleiro[1][0] === tabuleiro[1][2]) {
-        console.log('ganhou')
+        console.log(`${jogador.nome} ganhou!`)
     } else if (tabuleiro[2][0] !== 0 && tabuleiro[2][0] === tabuleiro[2][1] && tabuleiro[2][0] === tabuleiro[2][2]){
-        console.log('ganhou')
+        console.log(`${jogador.nome} ganhou!`)
     } else if (tabuleiro[0][0] !== 0 && tabuleiro[0][0] === tabuleiro[1][0] && tabuleiro[0][0] === tabuleiro[2][0]){
-        console.log('ganhou')
+        console.log(`${jogador.nome} ganhou!`)
     } else if (tabuleiro[0][1] !== 0 && tabuleiro[0][1] === tabuleiro[1][1] && tabuleiro[0][1] === tabuleiro[2][1]){
-        console.log('ganhou')
+        console.log(`${jogador.nome} ganhou!`)
     } else if (tabuleiro[0][2] !== 0 && tabuleiro[0][2] === tabuleiro[1][2] && tabuleiro[0][2] === tabuleiro[2][2]){
-        console.log('ganhou')
+        console.log(`${jogador.nome} ganhou!`)
     } else if (tabuleiro[0][0] !== 0 && tabuleiro[0][0] === tabuleiro[1][1] && tabuleiro[0][0] === tabuleiro[2][2]){
-        console.log('ganhou')
+        console.log(`${jogador.nome} ganhou!`)
     } else if (tabuleiro[0][2] !== 0 && tabuleiro[0][2] === tabuleiro[1][1] && tabuleiro[0][2] === tabuleiro[2][0]){
-        console.log('ganhou')
+        console.log(`${jogador.nome} ganhou!`)
     }
 }
 
